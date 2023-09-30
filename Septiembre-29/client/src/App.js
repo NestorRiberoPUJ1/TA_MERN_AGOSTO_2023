b06a552d82142c3fff1bd9b94169b9a47278dff2
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2023-09-29 19:21:42 -0500

    update

---
+++ Septiembre-29/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Componente from './components/Componente';
+
+function App() {
+  return (
+    <div className="App">
+      <Componente />
+      <Componente />
+      <Componente />
+    </div>
+  );
+}
+
+export default App;
