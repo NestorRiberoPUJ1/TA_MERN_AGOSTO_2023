a5ade3a9d667401973534e8c3a447d3169e5f707
NestorRiberoPUJ1 <67485144+NestorRiberoPUJ1@users.noreply.github.com>
2023-10-23 18:14:09 -0500

    Update ToDoList.jsx

--- Octubre-16/client/src/components/ToDoList/ToDoList.jsx
+++ Octubre-16/client/src/components/ToDoList/ToDoList.jsx
@@ -56,11 +56,22 @@ const ListElement = ({ item, setToDoList, index }) => {
         })
     }
 
+
+    const handleDelete = () => {
+        setToDoList((prevValue) => {
+            const aux = [...prevValue];
+            const newList = aux.filter((item, idx) => idx !== index);
+            return newList;
+        })
+    }
+
+
     return (
         <Fragment>
             <li style={{ color: elementColor }} className={item.checked ? styles.checkedElement : null}>
                 {item.name}
                 <input type="checkbox" checked={item.checked || false} onChange={handleCheck} />
+                <button onClick={handleDelete}>Delete</button>
             </li>
         </Fragment>
     )
@@ -70,15 +81,83 @@ const ToDoList = () => {
 
     const [toDoList, setToDoList] = useState([]);
 
+    const [filter, setFilter] = useState("");
+    const [category, setCategory] = useState("ALL");
+    const [status, setStatus] = useState("ALL");
+
     useEffect(() => {
         console.log(toDoList);
     }, [toDoList])
 
+
+    const filteredTasks = () => {
+        let filtered = toDoList.filter((value, indice, lista) => {
+            return value.name.includes(filter);
+        })
+        filtered = filtered.filter((value, indice, lista) => {
+            if (category === "ALL") {
+                return true;
+            }
+            return value.category === category;
+        })
+        filtered = filtered.filter((value, indice, lista) => {
+            let checkStatus = value.checked || false;
+            console.log(checkStatus, status);
+            if (status === "ALL") {
+                return true;
+            }
+            else if (status === "COMPLETED") {
+                return checkStatus;
+            }
+            return !checkStatus;
+        })
+
+        return filtered;
+    }
+
+
+    const completedTasks = () => {
+        const completed = toDoList.filter((item) => item.checked === true).length;
+        const total = toDoList.length;
+        return `#${completed}/${total}`;
+    }
+
+    const handleDeleteCompleted = () => {
+        setToDoList((prevValue) => {
+            const pending = prevValue.filter((item) => !item.checked);
+            return pending;
+        })
+    }
+
+    useEffect(() => {
+        filteredTasks();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter])
+
+
     return (
         <Fragment>
             <AddForm setToDoList={setToDoList} />
+            <button onClick={handleDeleteCompleted}>Borrar completadas</button>
+            <hr />
+            <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+            <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <option value="ALL" >Todos</option>
+                {categorias.map((item, idx) => {
+                    return (
+                        <option value={item.value} key={item.label + idx}>{item.label}</option>
+                    )
+                })}
+            </select>
+            <select value={status} onChange={(e) => setStatus(e.target.value)}>
+                <option value="ALL" >Todos</option>
+                <option value={"COMPLETED"} >Finalizados</option>
+                <option value={"PENDING"} >Pendientes</option>
+            </select>
+            <hr />
+            <h3>{completedTasks()}</h3>
             <ul>
-                {toDoList.map((item, index) => {
+                {filteredTasks().map((item, index) => {
                     return (
                         <ListElement item={item} key={item.name + index} setToDoList={setToDoList} index={index} />
                     )
